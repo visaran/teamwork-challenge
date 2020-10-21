06b4531e2931f10d53dfbe67d6a4ab61a3d3595c
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 18:39:17 -0300

    refactor: getting users and planets

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,19 +1,40 @@
+import moment from "moment-timezone";
 import api from "../../api";
 
 const state = () => ({
-  all: []
+  all: [],
+  count: 0
 });
 
 const mutations = {
-  GET_ALL(state, all) {
+  SET_USERS(state, all) {
     state.all = all;
+  },
+  SET_COUNT(state, count) {
+    state.count = count;
   }
 };
 
 const actions = {
-  getAll({ commit }) {
-    return api.getUsers().then(res => {
-      commit("GET_ALL", res.data.results);
+  async getAll({ commit }, { page, search }) {
+    return api.getUsers(page, search).then(async res => {
+      commit("SET_COUNT", res.data.count);
+      const results = await Promise.all(
+        res.data.results.map(async item => {
+          const { data } = await api.getPlanetByUrl(item.homeworld);
+          return {
+            ...item,
+            planet: data,
+            created: moment
+              .tz(item.created, "Europe/Dublin")
+              .format("DD/MM/YY hh:mm A"),
+            edited: moment
+              .tz(item.edited, "Europe/Dublin")
+              .format("DD/MM/YY hh:mm A")
+          };
+        })
+      );
+      commit("SET_USERS", results);
     });
   }
 };
