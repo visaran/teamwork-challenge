f635952be28b4c8df8302aeada657165c4374136
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 01:11:52 -0300

    feat: store, router, veutify and pollyfill

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import vuetify from "./plugins/vuetify";
+import store from "./store";
+import router from "./router";
 
 new Vue({
-  el: '#app',
+  vuetify,
+  el: "#app",
+  store,
+  router,
   render: h => h(App)
-})
+});
