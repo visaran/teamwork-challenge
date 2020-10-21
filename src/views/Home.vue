968dd2ce7dc3863451dbc37b226310133b0d8411
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 01:14:44 -0300

    feat: view Home

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="px-4 text-center">
+    <h1 class="mb-4">Teamwork Front-end Challenge</h1>
+    <h2>Navigate pages through the bottom navigation</h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss"></style>
