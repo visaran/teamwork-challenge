fcb06ab1275a6c77afd1c16e60753b6b3617da95
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 18:40:31 -0300

    feat: fetching and paginating

--- src/components/PeopleTable.vue
+++ src/components/PeopleTable.vue
@@ -2,31 +2,41 @@
   <div>
     <v-card>
       <v-card-title>
-        Search by name
+        List of people
         <v-spacer></v-spacer>
         <v-text-field
           v-model="search"
           append-icon="mdi-magnify"
-          label="Search"
+          label="Search by name"
           single-line
           hide-details
+          @keyup="searchByName"
+          :disabled="isLoading"
         ></v-text-field>
       </v-card-title>
       <v-data-table
         :headers="headers"
         :items="allUsers"
-        :items-per-page="5"
+        :items-per-page="10"
         class="elevation-1"
-        :search="search"
-        :loading="!allUsers.length"
+        disable-filtering
+        :loading="isLoading"
+        hide-default-footer
         loading-text="Loading... Please wait"
       >
         <template v-slot:[`item.homeworld`]="{ item }">
-          <a @click="openDialog(item.homeworld)">
-            {{ item.homeworld }}
+          <a @click="openDialog(item.planet)">
+            {{ item.planet.name }}
           </a>
         </template>
       </v-data-table>
+      <div class="text-center pt-2">
+        <v-pagination
+          v-model="page"
+          @input="changePage(page)"
+          :length="pages"
+        ></v-pagination>
+      </div>
     </v-card>
 
     <v-snackbar v-model="snackbar">
@@ -48,6 +58,11 @@ import api from "../api";
 export default {
   data() {
     return {
+      page: 1,
+      search: "",
+      isLoading: false,
+      itemsPerPage: 10,
+      searchDebounceTimer: null,
       snackbar: false,
       search: "",
       planetNames: [],
@@ -69,34 +84,40 @@ export default {
   },
   computed: {
     ...mapState({
-      allUsers: state => state.user.all
+      allUsers: state => state.user.all,
+      pages: state => Math.ceil(state.user.count / 10)
     })
   },
   methods: {
-    fetchUsers() {
-      this.$store.dispatch("user/getAll").catch(err => {
-        this.snackbar = true;
-      });
-    },
-    openDialog(url) {
-      this.$store.commit("planet/SET_LOADING", true);
+    fetchUsers(page, search) {
+      this.isLoading = true;
 
-      api
-        .getPlanetByUrl(url)
-        .then(res => {
-          this.$store.commit("planet/SET_PLANET", res.data);
-        })
-        .catch(() => {
+      this.$store
+        .dispatch("user/getAll", { page, search })
+        .catch(err => {
           this.snackbar = true;
         })
         .finally(() => {
-          this.$store.commit("planet/SET_LOADING", false);
+          this.isLoading = false;
         });
+    },
+    changePage(page) {
+      this.fetchUsers(page, this.search);
+    },
+    searchByName() {
+      clearTimeout(this.searchDebounceTimer);
+      this.searchDebounceTimer = setTimeout(() => {
+        this.page = 1;
+        this.fetchUsers(this.page, this.search);
+      }, 1000);
+    },
+    openDialog(planet) {
+      this.$store.commit("planet/SET_PLANET", planet);
       this.$store.commit("dialog/TOGGLE", true);
     }
   },
   mounted() {
-    this.fetchUsers();
+    this.fetchUsers(this.page, this.search);
   }
 };
 </script>
