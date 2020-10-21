2e500111fc48fd89554338d2154b5616bf4bad15
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 01:14:53 -0300

    feat: view People

---
+++ src/views/People.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-container class="pt-8">
+      <people-table />
+    </v-container>
+    <planet-dialog />
+  </div>
+</template>
+
+<script>
+import PeopleTable from "../components/PeopleTable";
+import PlanetDialog from "../components/PlanetDialog";
+
+export default {
+  components: {
+    PeopleTable,
+    PlanetDialog
+  }
+};
+</script>
+
+<style lang="scss"></style>
