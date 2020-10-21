e9c66d08f1306b10eb07668da663126a0feb3552
Vinicius Saran <viniciussaran@gmail.com>
2020-10-21 01:10:23 -0300

    feat: layout setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <div id="app">
-    {{ msg }}
-  </div>
+  <v-app id="app">
+    <v-card color="grey lighten-4" flat tile>
+      <v-toolbar dense>
+        <v-toolbar-title>Teamwork Front-end Challenge</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon to="/">
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn icon to="/people">
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+
+    <div>
+      <router-view></router-view>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      msg: "oioio"
-    };
+    return {};
   }
 };
 </script>
